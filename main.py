2e3827172d4e137458254f5ff0bf2b0c98721861
Dorothy Luo <dorothy.luo88@gmail.com>
2020-10-17 16:19:56 -0400

    cheese
    updated the commands

--- main.py
+++ main.py
@@ -9,37 +9,54 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def stockprofile(ctx, *, arg):
     current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
-    currentc = current.json()['c']
-    await ctx.send(':moneybag: The current Price of ' + arg.upper() + ' is $' + str(currentc))
-    currentl = current.json()['l']
-    await ctx.send(':moneybag: The low price of ' + arg.upper() + ' was $' + str(currentl))
-    currento = current.json()['o']
-    await ctx.send(':moneybag: The open price of ' + arg.upper() + ' is $' + str(currento))
+    await ctx.send(':moneybag: The current Price of ' + arg.upper() + ' is $' + str(current.json()['c']))
+    await ctx.send(":moneybag: The high price of " + arg.upper() + " was $" + str(current.json()['h']))
+    await ctx.send(':moneybag: The low price of ' + arg.upper() + ' was $' + str(current.json()['l']))
+    await ctx.send(':moneybag: The open price of ' + arg.upper() + ' is $' + str(current.json()['o']))
     await ctx.send(":moneybag: The previous closing price of " + arg.upper() + " was $" + str(current.json()['pc']))
+    await ctx.send(":bar_chart: The volume data of " + arg.upper() + " is " + str(current.json()['v']))
+    await ctx.send(":clock3: The time stamp of " + arg.upper() + " is " + str(current.json()['t']))
+    await ctx.send(":bangbang: The request status of " + arg.upper() + " is " + str(current.json()['s']))
 
 @bot.command()
-async def lowprice(ctx, *, arg):
+async def lowprice(ctx, arg):
     current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
-    currentl = current.json()['l']
-    await ctx.send(':moneybag: The low price of ' + arg.upper() + ' was $' + str(currentl))
+    await ctx.send(':moneybag: The low price of ' + arg.upper() + ' was $' + str(current.json()['l']))
 
 @bot.command()
-async def openprice(ctx, *, arg):
+async def openprice(ctx, arg):
     current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
-    currento = current.json()['o']
-    await ctx.send(':moneybag: The open price of ' + arg.upper() + ' is $' + str(currento))
+    await ctx.send(':moneybag: The open price of ' + arg.upper() + ' is $' + str(current.json()['o']))
 
 @bot.command()
 async def previousclose(ctx, arg):
     current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
     await ctx.send(":moneybag: The previous closing price of " + arg.upper() + " was $" + str(current.json()['pc']))
-    
+
 @bot.command()
 async def currentprice(ctx, arg):
     current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
-    currentc = current.json()['c']
-    await ctx.send(':moneybag: The current Price of ' + arg.upper() + ' is $' + str(currentc))
+    await ctx.send(':moneybag: The current Price of ' + arg.upper() + ' is $' + str(current.json()['c']))
 
+@bot.command()
+async def highprice(ctx, arg):
+    current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
+    await ctx.send(":moneybag: The high price of " + arg.upper() + " was $" + str(current.json()['h']))
+
+@bot.command()
+async def volumedata(ctx, arg):
+    current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
+    await ctx.send(":bar_chart: The volume data of " + arg.upper() + " is " + str(current.json()['v']))
+
+@bot.command()
+async def timestamp(ctx, arg):
+    current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
+    await ctx.send(":clock3: The time stamp of " + arg.upper() + " is " + str(current.json()['t']))
+
+@bot.command()
+async def responsestatus(ctx, arg):
+    current = requests.get('https://finnhub.io/api/v1/quote?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
+    await ctx.send(":bangbang: The request status of " + arg.upper() + " is " + str(current.json()['s']))
 
 @bot.command()
 async def eatmyASS(ctx):
