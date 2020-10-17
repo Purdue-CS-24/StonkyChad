4323dd18fa136d43562f57f842e365bf51503e78
Vidhi Singh <singh726@purdue.edu>
2020-10-17 14:31:56 -0500

    this bot FUCKING WORKS LETS FUCKING GOOOOOOOO

--- main.py
+++ main.py
@@ -4,31 +4,19 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-client = discord.Client()
-
 @bot.command()
 async def analysis(ctx, *, arg):
     await ctx.send(arg)
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+@bot.command()
+async def eatmyASS(ctx):
+    await ctx.send('i dont have a mouth')
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
-        
-    if message.content.startswith('!analysis'):
-        msg = 'THIS BOT DONT WORK'
-        await message.channel.seng(msg)
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-client.run(config.token)
+bot.run(config.token)
