c474dcc3f933776b4b161053839488cfd8557315
Ananya <xananya.singhx@gmail.com>
2020-10-17 15:22:10 -0700

    added company profile

--- main.py
+++ main.py
@@ -28,6 +28,23 @@ async def stockprofile(ctx, *, arg):
     # await ctx.send(":clock3: The time stamp of " + arg.upper() + " is " + str(current.json()['t']))
     # await ctx.send(":bangbang: The request status of " + arg.upper() + " is " + str(current.json()['s']))
 
+@bot.command()
+async def companyprofile(ctx, *, arg):
+    profile = requests.get('https://finnhub.io/api/v1//stock/profile2?symbol=' + arg.upper() + '&token=bto4nln48v6v7atimad0')
+
+    displaymsg = "The name of the company of " + arg.upper() + " is " + str(profile.json()['name']) + "\n" + \
+                 "The country of company's headquarter is " + str(profile.json()['country']) + "\n" + \
+                 "The currency used in company filings is " + str(profile.json()['currency']) + "\n" + \
+                 "The listed exchange is " + str(profile.json()['exchange']) + "\n" + \
+                 "The company symbol/ticker as used on the listed exchange is " + str(profile.json()['ticker']) + "\n" + \
+                 "The IPO date of the company is " + str(profile.json()['ipo']) + "\n" + \
+                 "The market capitalization of the company is " + str(profile.json()['marketCapitalization']) + "\n" + \
+                 "The number of outstanding shares of the company is " + str(profile.json()['shareOutstanding']) + "\n" + \
+                 "The company phone number is " + str(profile.json()['phone']) + "\n" + \
+                 "The company website is " + str(profile.json()['weburl']) + "\n" + \
+                 "The industry classification of the company is " + str(profile.json()['finnhubIndustry']) + "\n"
+
+    await ctx.send(displaymsg)
 
 @bot.command()
 async def lowprice(ctx, arg):
